7248f57cc70c7e683aa7cb2c01164bf28ebe12dc
HuyB1910227 <huyb1910227@student.ctu.edu.vn>
2022-09-15 16:03:42 +0700

    add website

---
+++ assets/js/ham-tien-ich.js
@@ -0,0 +1,13 @@
+alert('Hi, there!');
+function tinh_trung_binh_cong(a, b, c) {
+    return (a + b + c) / 3;
+}
+function tinh_binh_phuong(number) {
+    return number * number;
+}
+function tinh_lap_phuong(number){
+    return number * number * number;
+}
+function hien_thi_cau_chao_mung(hoten){
+    document.write(`Xin chào mừng ${hoten} đã ghé thăm trang web của chúng tôi! 🥰🥰🥰`);
+}
